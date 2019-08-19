f1203a6089b713398b0c118d250108eb0a7ba021
rainbow954 <rainbow954@163.com>
2019-08-19 17:50:12 +0800

    update build script for deploy on k8s

--- eventuate-local-console/eventuate-local-console-server/docker/build.sh
+++ eventuate-local-console/eventuate-local-console-server/docker/build.sh
@@ -9,4 +9,14 @@ git archive --format=tar.gz -o eventuate-local-console-server/docker/build/event
 
 cd eventuate-local-console-server/docker
 docker build -t eventuate-local-console-test .
+
+sudo docker login --username=rainbow954@163.com registry.cn-hangzhou.aliyuncs.com --password dockerhub@iotbull.com
+sudo docker tag eventuate-local-console-test registry.cn-hangzhou.aliyuncs.com/iotbull/eventuate-local-console:0.30.4
+sudo docker push registry.cn-hangzhou.aliyuncs.com/iotbull/eventuate-local-console:0.30.4
+ 
+
+
+
+
+
  
--- new-cdc/eventuate-local-java-cdc-sql-service/build-docker.sh
+++ new-cdc/eventuate-local-java-cdc-sql-service/build-docker.sh
@@ -6,3 +6,8 @@ if [ $(ls build/libs/*SNAPSHOT.jar | wc -l) != "1" ] ; then
 fi
 
 docker build -t test-eventuate-local-java-new-cdc-mysql-service .
+
+sudo docker login --username=rainbow954@163.com registry.cn-hangzhou.aliyuncs.com --password dockerhub@iotbull.com
+sudo docker tag test-eventuate-local-java-new-cdc-mysql-service:latest registry.cn-hangzhou.aliyuncs.com/iotbull/test-eventuate-local-java-new-cdc-mysql-service:0.30.4
+sudo docker push registry.cn-hangzhou.aliyuncs.com/iotbull/test-eventuate-local-java-new-cdc-mysql-service:0.30.4
+
